0fbbba4a738517db7adb8a7af8fb524010e1e4d5
Roman Luts <rluts@lohika.com>
2021-11-12 17:58:59 +0200

    check

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ class RecentPagesBot(BaseBot):
     AND user_editcount >= 150 order by user_editcount desc;
     """
     links = ["User:RLutsBot/Активні", "User:RLutsBot/Редагування"]
-    header = "Список користувачів, кількість редагувань яких не менша 100"
+    header = "Список користувачів, кількість редагувань яких не менша 150"
     title = "Користувач:RLutsBot/Редагування"
     frequency = "щоденно"
 
