7a7fd70974f2f8aea2cbd0d095b57ae86282558a
Roman Luts <rluts@lohika.com>
2021-11-12 23:06:26 +0200

    check

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 cd /data/project/rluts/statistic_bot/
 pip3 install -r requirements.txt
-python3 /data/project/rluts/statistic_bot/run.py $1
+python3 /data/project/rluts/statistic_bot/run.py $1 -dir:/data/project/rluts/
